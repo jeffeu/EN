2a4f8b63768a8cc153ace63b48bda74bb013ec10
jeffeu <jefferson.florencio@live.com>
2013-10-01 14:18:38 -0300

    AddLinkToAutocomplete

--- EnlaceNoivas/EnlaceNoivas/Scripts/Layout/layout.js
+++ EnlaceNoivas/EnlaceNoivas/Scripts/Layout/layout.js
@@ -32,12 +32,16 @@ $(document).ready(function () {
             noResults: "", results: ""
         }
     }).data( "autocomplete" )._renderItem = function( ul, item ) {
-        //var inner_html = '<a><div class="list_item_container"><div class="label">' + item.label + '</div></div><div class="label">' + item.city + '</div></div><div class="label">' + item.street + ', '+item.number+'</div></div></a>';
-        //var innet_html = '<a><div class="row"><div class="col-lg-2"><div class="image"><img width="70px" height="75px" src="' + item.image + '"></div></div><div class="row">  </div></div></a>'
         var inner_html = '<a><div class="row"></div><div class="col-lg-4" id="image"><img width="70" height="75" src="' + item.image + '"/></div><div class="col-lg-8"><div class="row"><h4>' + item.label + '</h4></div><div class="row"><p>' + item.city + '</p><p>' + item.street + ' ,' + item.number + '</p></div></div></a>';
-        return $("<li></li>")
+        return $('<a href="/Provider/'+replaceAll(item.label," ","-")+'"><li></li></a>')
             .data( "item.autocomplete", item )
             .append(inner_html)
             .appendTo( ul );
     };
 });
+function replaceAll(text, charb, chara){
+    while(text.indexOf(charb) >= 0) {
+        text = text.replace(charb,chara);
+    }
+    return text;
+}
